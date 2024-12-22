6144eb60abf76201e28f6b32475de5506520c838
mina-y-khalil <mina.y.khalil90@gmail.com>
2024-12-22 15:23:39 -0800

    updated

--- SCE tables.sql
+++ SCE tables.sql
@@ -1,26 +1,27 @@
+--Tables
+
 DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS invoices;
 
-
-CREATE TABLE projects(
-    id INTEGER PRIMARY KEY,
-project_number INTEGER(10) NOT NULL,
-project_name VARCHAR(100) NOT NULL,
-pm VARCHAR(100) NOT NULL
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_number UNIQUE INTEGER NOT NULL,
+    project_name VARCHAR(100) NOT NULL,
+    pm VARCHAR(100) NOT NULL
 );
 
-
-CREATE TABLE tasks(
-    id INTEGER PRIMARY KEY,
-task_number VARCHAR(3),
-task_name VARCHAR(200),
-task_budget INTEGER(9 , 2)
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task_number VARCHAR(3) NOT NULL,
+    task_name VARCHAR(200) NOT NULL,
+    task_budget REAL NOT NULL
 );
-
 
 CREATE TABLE invoices (
-    id INTEGER PRIMARY KEY,
-invoice_number INTEGER (20),
-invoice_month VARCHAR(15),
-
-)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    invoice_number INTEGER NOT NULL,
+    invoice_month VARCHAR(15) NOT NULL,
+    invoice_year VARCHAR(15) NOT NULL,
+    FOREIGN KEY (task_billed_amount) REFERENCES tasks(task_number)
+);
