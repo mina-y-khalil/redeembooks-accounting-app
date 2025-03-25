951686603f506b44d5263492bed098a770d9a890
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-03-24 22:32:53 -0700

    Organize Express project structure and update file layout

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-let port = 8000;
+const port = 8000;
 app.listen(port, () => {
-  console.log("Server running on port ", port);
+  console.log(`Server running on port ${port}.`);
 });
