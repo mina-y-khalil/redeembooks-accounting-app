f89aca945a27b15128a3ec8a9c9731089205a8c1
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-03-25 21:25:50 -0700

    Organize Express project structure and update file layout

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello from RedeemBooks</h1>");
+});
+
+app.get("/about", (req, res, next) => {
+  res.send("<h1>About us</h1>");
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
