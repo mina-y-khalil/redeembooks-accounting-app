b380dfb54fef7993e52717c9dd14a3cfc3a44497
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-08-01 18:08:27 -0700

    Finalize db models and successfully run migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,13 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .company import Company
+from .user_company import UserCompany
+from .approver import Approver
+from .vendor import Vendor
+from .category import Category
+from .invoice import Invoice
+from .payment import Payment
+from .payment_batch import PaymentBatch
+from .bank_balance import BankBalance
+from .audit_log import AuditLog
